dd320fe6083a6a061eedd58ae3abdc5a4dc577a7
Alexander Schwartz <alexander.schwartz@gmx.net>
2018-08-31 17:37:08 +0200

    added a demo endpoint

--- src/main/java/de/ahus1/springprometheus/rest/RestEndpoint.java
+++ src/main/java/de/ahus1/springprometheus/rest/RestEndpoint.java
@@ -1,6 +1,8 @@
 package de.ahus1.springprometheus.rest;
 
 import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +17,17 @@ public class RestEndpoint {
 
     private static final Logger log = org.slf4j.LoggerFactory.getLogger(RestEndpoint.class);
     private final ServiceClass serviceClass;
+    private final Counter myOperationCounterSuccess;
 
-    public RestEndpoint(ServiceClass serviceClass) {
+    public RestEndpoint(ServiceClass serviceClass, MeterRegistry registry) {
         this.serviceClass = serviceClass;
+
+        myOperationCounterSuccess = Counter
+                .builder("myOperation")
+                .description("a description for humans")
+                .tags("result", "success")
+                .register(registry);
+
     }
 
     @GetMapping("/countedCall")
@@ -27,6 +37,12 @@ public class RestEndpoint {
         delay += serviceClass.doSomething();
         delay += serviceClass.callExternal();
         return "waited: " + delay + " ms";
+    }
+
+    @GetMapping("/demo")
+    public String apiUse() {
+        myOperationCounterSuccess.increment();
+        return "OK";
     }
 
 }
